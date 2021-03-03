a5abbfbb2dbc8d0e68285b2c352c4af130465113
jhanxo <38596838+jhanxo@users.noreply.github.com>
2021-03-02 21:04:51 -0500

    migrations

--- app/__init__.py
+++ app/__init__.py
@@ -12,5 +12,13 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+def __init__(self, id, first_name, last_name,username, password):
+    self.first_name = first_name
+    self.id = id
+    self.first_name = first_name
+    self.last_name = last_name
+    self.username = username
+    self.password = password
+
 app.config.from_object(Config)
 from app import views
