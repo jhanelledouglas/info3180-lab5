050f022e31a0520e647f517099d54a1b8f11d005
jhanxo <38596838+jhanxo@users.noreply.github.com>
2021-03-04 20:32:03 -0500

    soon

--- app/__init__.py
+++ app/__init__.py
@@ -11,14 +11,10 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+login_manager.login_message_category = "info"
 
-def __init__(self, id, first_name, last_name,username, password):
-    self.first_name = first_name
-    self.id = id
-    self.first_name = first_name
-    self.last_name = last_name
-    self.username = username
-    self.password = password
+
 
 app.config.from_object(Config)
 from app import views
+
--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
+    remember_me = BooleanField('Remember me')
